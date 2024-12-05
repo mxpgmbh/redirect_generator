0db0826b9da3741668355db245812c736670e0a0
mxpgmbh <62594408+mxpgmbh@users.noreply.github.com>
2024-12-05 10:38:39 +0100

    Set next major version

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'r.kiefhaber@mxp.de',
     'state' => 'beta',
     'clearCacheOnLoad' => true,
-    'version' => '2.0.0',
+    'version' => '3.0.0',
     'constraints' =>
         [
             'depends' => [
